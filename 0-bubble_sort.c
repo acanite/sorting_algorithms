a2c775be864db0fc6cab26094731207ca63c4c22
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2023-01-20 13:36:20 +0100

    task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,35 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array using the bubble algorithm
- * @array: an array
- * @size: size of the array 'number of elements in it'
- * Return: void
+ * bubble_sort - This function sort an array of integers
+ * using bubble algo method
+ *@array: array of unsorted integers
+ *@size: size of the array
+ *Author: Thaoban Abdrasheed
+ *
+ * Return: nothing to return
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n;
-	int big_val, flag;
+int temp;
+size_t i, j;
 
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-	{
-		flag = 0;
-		for (n = 0; n < size - 1; n++)
-		{
-			if (array[n] > array[n + 1])
-			{
-				big_val = array[n];
-				array[n] = array[n + 1];
-				array[n + 1] = big_val;
-				flag = 1;
-				print_array(array, size);
-			}
-		}
-		if (flag == 0)
-			return;
-	}
-
+if (array == NULL || size < 2)
+{
+return;
+}
+for (i = 0; i != size; i++)
+{
+for (j = 0; j != size - 1; j++)
+{
+if (array[j] >= array[j + 1])
+{
+temp = array[j + 1];
+array[j + 1] = array[j];
+array[j] = temp;
+print_array(array, size);
+}
+}
+}
 }
